74879edde15ce2928dbf5f4f47dafb7b5f925af7
Beginnerzzq <307545449@qq.com>
2021-07-09 20:18:00 +0800

    把本地工程添加到git

---
+++ Git-test/src/test/git_test.java
@@ -0,0 +1,14 @@
+package test;
+
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+        System.out.println("gittest");
+    }
+}
